5de5caa789e99e561ec1acaf7acc1de17677ba6f
Luis <m.luishernandez@outlook.com>
2021-06-19 22:00:59 -0400

    Prepared questions. Upload files

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Which video game company develop Apex Legends:",
+    choices: ["EA", "Activision", "Respawn", "Bethesda"],
+    answer: "Respawn"
+  },
+  {
+    title: "What is the real name of Scorpion from the Mortal Kombat franchise:",
+    choices: ["Gojo Satoru", "Hanzo Hasashi", "Shinzo Murasame", "Kamaboko Gompachiro"],
+    answer: "Hanzo Hasashi"
+  },
+  {
+    title: "What is the name of Kratos son from the God Of War franchise:",
+    choices: ["Atreyu", "Arceus", "Boy", "Atreus"],
+    answer: "Atreus"
+  },
+  {
+    title: "What is the most selled game of all time:",
+    choices: ["Minecraft", "GTA", "Tetris", "Mario"],
+    answer: "Minecraft"
+  },
+  {
+    title: "How many first generation Pokemons are there:",
+    choices: ["151", "152", "147", "200"],
+    answer: "151"
+  }
+];
