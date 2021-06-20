cc50019d12853cb41fc43a721b11bf703c1062aa
Luis <m.luishernandez@outlook.com>
2021-06-20 17:30:55 -0400

    Made the title and questions appear

--- assets/js/logic2.js
+++ assets/js/logic2.js
@@ -1,25 +1,31 @@
-//Start quiz variables
+//startQuiz variables
 var startScreen = document.getElementById('start-screen');
 var questionScreen = document.getElementById('questions');
 var startBtn = document.getElementById('start');
 
-//Quiz timer variables
+//quizTimer variables
 var timerCount = document.getElementById('time');
 var seconds = 15
 
+//getQuestion variables
+var title = document.getElementById('question-title');
+var choiceEl = document.getElementById('choices');
+var questionIndex = 0;
+
+startQuiz();
 //Function that start quiz
 function startQuiz() {
     //Start button will set the attribute hide to start-screen while removing the class from the question screen which will appear after clicked
     startBtn.addEventListener('click', function () {
      startScreen.setAttribute('class', "hide");
      questionScreen.removeAttribute('class');
-     quizTimer()
+     quizTimer();
+     getQuestions();
     }); 
 }
 
 //Function that starts the timer
 function quizTimer() {
-
     
     var timerInterval = setInterval(function () {
         //Span with the id time will have the text content of the seconds variable
@@ -29,13 +35,26 @@ function quizTimer() {
         //If seconds is less than 0 clear the timerInterval
         if (seconds < 0) {
             //The clearInterval() method clears a timer set with the setInterval() method.
-            clearInterval(timerInterval)
-            
+            clearInterval(timerInterval);
         }
-        
-    },1000)
-
-    
+        // Set the speed in seconds of how the timer is going to work
+    },1000);
 }
 
-startQuiz()
+
+function getQuestions() {
+    //This variable holds the first index of the questions array which is 0
+    var currentQuestion = questions[questionIndex];
+
+    //Display the title of the question
+    title.textContent = currentQuestion.title
+
+    //The forEach() method executes a provided function once for each array element.
+    //In simpler words it will take currentQuestion, we traversed to choices and then for each choice we will create an element buttoon with the element text content and append it to choiceEl
+    currentQuestion.choices.forEach(function (element) {
+            var choiceBtn = document.createElement('button');
+            choiceBtn.textContent = element
+            choiceEl.appendChild(choiceBtn);  
+    });
+    
+}
